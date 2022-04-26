342cb5b3d96f68a92be9c61954d503579ef89b77
Niccolò <n.balzarotti2@campus.unimib.it>
2022-04-26 10:33:19 +0200

    create spring server

---
+++ bbrmanagementwork/src/main/java/com/kaisengroup/bbrmanagementwork/BbrmanagementworkApplication.java
@@ -0,0 +1,13 @@
+package com.kaisengroup.bbrmanagementwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbrmanagementworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbrmanagementworkApplication.class, args);
+	}
+
+}
---
+++ bbrmanagementwork/src/test/java/com/kaisengroup/bbrmanagementwork/BbrmanagementworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaisengroup.bbrmanagementwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbrmanagementworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
